138fe347a0555cc68cb2b7010ce2792d51ccb728
hacca <horiuchi@hacca.cc>
2018-11-25 10:48:18 +0900

    no message

--- footer.php
+++ footer.php
@@ -24,29 +24,6 @@
 <div id="drawer_menu" class="md_drawer__menu">
     <div class="md_drawer__contents">
         <div class="sidr_slide__close"><span></span><span></span></div>
-        <div class="md_slide__nav">
-            <h2 class="md_slide__navLogo"><a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo_with.svg" alt="ホテル花屋" width="200"></a></h2>
-            <ul class="fz_15 md_slide__navColumn">
-                <li><a href="<?php echo home_url(); ?>">ホーム</a></li>
-                <li><a href="<?php echo home_url(); ?>/about/">ホテル花屋について</a></li>
-                <li><a href="<?php echo home_url(); ?>/roomtype/">客室のご案内</span></a></li>
-                <li><a href="<?php echo home_url(); ?>/access/">交通アクセス</a></li>
-                <li><a href="<?php echo home_url(); ?>/sightseeing/">周辺情報・観光案内</a></li>
-                <li><a href="<?php echo home_url(); ?>/contact/">お問い合わせ</a></li>
-            </ul>
-            <div class="md_slide__navOther">
-                <a href="<?php echo home_url(); ?>/hanaiori/"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo_hanaan.svg" alt="花庵" width="150"></a>
-            </div>
-        </div>
-        <div class="md_slide__navFooter">
-            <p class="fz_13">〒646-0028 <br>和歌山県田辺市高雄１丁目２番５３号</p>
-            <p class="fz_13">TEL：0739-22-3877FAX：0739-25-1400</p>
-            <p class="fz_13">info@hotel-hanaya.com</p>
-            <p class="fz_13 md_slide__navSub"><a href="<?php echo home_url(); ?>/privacypolicy/">Privacy Policy</a></p>
-            <div class="fz_13 md_slide__navSns">
-                <a href="https://www.facebook.com/%E3%83%93%E3%82%B8%E3%83%8D%E3%82%B9%E3%83%9B%E3%83%86%E3%83%AB%E8%8A%B1%E5%B1%8B-Hotel-Hanaya-272838719485086/"><i class="fab fa-facebook-f"></i></a>
-            </div>
-        </div>
     </div>
 </div>
 
