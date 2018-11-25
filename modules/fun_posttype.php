e256a48263951e9176b89a0d9a693b67b0c528a8
hacca <horiuchi@hacca.cc>
2018-11-25 17:23:53 +0900

    no message

--- modules/fun_posttype.php
+++ modules/fun_posttype.php
@@ -8,13 +8,13 @@
     //     コンテンツ カスタムポスト
     // ---------------------------------------------
 
-add_action( 'init', 'create_post_type_article' );
-function create_post_type_article() {
+add_action( 'init', 'create_post_type_item' );
+function create_post_type_item() {
     $args = array(
-        'label' => '旅のサンプル帖',
+        'label' => 'アイテム',
         'labels' => array(
-            'singular_name' => '旅のサンプル帖',
-            'add_new_item' => '旅のサンプル帖の新規作成',
+            'singular_name' => 'アイテム',
+            'add_new_item' => 'アイテムの新規作成',
             'add_new' => '新規作成',
             'new_item' => '新規作成',
             'view_item' => '記事を表示',
@@ -26,16 +26,16 @@ function create_post_type_article() {
         'show_ui' => true,
         'query_var' => true,
         'hierarchical' => false,
-        'rewrite' => array( 'slug' => 'journey', 'with_front' =>false ),
+        'rewrite' => array( 'slug' => 'item', 'with_front' =>false ),
         'menu_position' => 5,
         'menu_icon' => 'dashicons-format-aside',
-        'supports' => array('title','editor','thumbnail'),
-        'has_archive' => 'journey',
+        'supports' => array('title','thumbnail'),
+        'has_archive' => 'item',
                 //posttypeにオリジナルの権限
-                //'capability_type' => array( 'journey', 'journeys' ),
+                //'capability_type' => array( 'item', 'items' ),
                 //'map_meta_cap' => true
     );
-    register_post_type('journey', $args);
+    register_post_type('item', $args);
     $args = array(
         'label' => 'カテゴリー',
         'labels' => array(
@@ -59,137 +59,17 @@ function create_post_type_article() {
             'edit_terms' => 'edit_others_posts'
         )
     );
-    register_taxonomy('journeycat', 'journey', $args);
+    register_taxonomy('itemcat', 'item', $args);
 }
-
-$args = array(
-    'label' => 'タグ',
-    'labels' => array(
-                    'name' => '関連タグ',// $post_type_object->label
-                ),
-    'public' => true,
-    'show_ui' => true,
-    'rewrite' => array( 'slug' => 'journeytag', 'with_front' =>false ),
-    'hierarchical' => false,
-    'query_var' => true,
-    'capabilities' => array(
-        'assign_terms' => 'edit_others_posts',
-        'edit_terms' => 'edit_others_posts'
-    )
-);
-register_taxonomy('journeytag', 'journey', $args);
-
-
-    //     ツアー カスタムポスト
-    // ---------------------------------------------
-
-add_action( 'init', 'create_post_type_tour' );
-function create_post_type_tour() {
-    $args = array(
-        'label' => 'ツアー',
-        'labels' => array(
-            'singular_name' => 'ツアー',
-            'add_new_item' => 'ツアーの新規作成',
-            'add_new' => '新規作成',
-            'new_item' => '新規作成',
-            'view_item' => '記事を表示',
-            'not_found' => '見つかりません',
-            'not_found_in_trash' => 'ゴミ箱にはありません',
-            'search_items' => '検索',
-        ),
-        'public' => true,
-        'show_ui' => true,
-        'query_var' => true,
-        'hierarchical' => false,
-        'rewrite' => array( 'slug' => 'tour', 'with_front' =>false ),
-        'menu_position' => 5,
-        'menu_icon' => 'dashicons-format-aside',
-        'supports' => array('title','editor','thumbnail'),
-        'has_archive' => 'tour',
-                //posttypeにオリジナルの権限
-                //'capability_type' => array( 'tour', 'tours' ),
-                //'map_meta_cap' => true
-    );
-    register_post_type('tour', $args);
-    $args = array(
-        'label' => 'カテゴリー',
-        'labels' => array(
-            'name' => 'カテゴリー',
-            'singular_name' => 'カテゴリー',
-            'search_items' => 'カテゴリーを検索',
-            'popular_items' => 'よく使われているカテゴリー',
-            'all_items' => 'すべてのカテゴリー',
-            'parent_item' => '親カテゴリー',
-            'edit_item' => 'カテゴリーの編集',
-            'update_item' => '更新',
-            'add_new_item' => '新規カテゴリーを追加',
-            'new_item_name' => '新しいカテゴリー',
-        ),
-        'public' => true,
-        'show_ui' => true,
-        'hierarchical' => true,
-        'rewrite' => array(true, 'with_front' => false),
-        'capabilities' => array(
-            'assign_terms' => 'edit_others_posts',
-            'edit_terms' => 'edit_others_posts'
-        )
-    );
-    register_taxonomy('tourcat', 'tour', $args);
-}
-
-$args = array(
-    'label' => 'タグ',
-    'labels' => array(
-                    'name' => '関連タグ',// $post_type_object->label
-                ),
-    'public' => true,
-    'show_ui' => true,
-    'rewrite' => array( 'slug' => 'tourtag', 'with_front' =>false ),
-    'hierarchical' => true,
-    'query_var' => true,
-    'capabilities' => array(
-        'assign_terms' => 'edit_others_posts',
-        'edit_terms' => 'edit_others_posts'
-    )
-);
-register_taxonomy('tourtag', 'tour', $args);
-
-
-$args = array(
-    'label' => '目的地',
-    'labels' => array(
-                    'name' => '目的地',// $post_type_object->label
-                ),
-    'public' => true,
-    'show_ui' => true,
-    'rewrite' => array( 'slug' => 'tour_destination', 'with_front' =>false ),
-    'hierarchical' => true,
-    'query_var' => true,
-    'capabilities' => array(
-        'assign_terms' => 'edit_others_posts',
-        'edit_terms' => 'edit_others_posts'
-    )
-);
-register_taxonomy('tour_destination', 'tour', $args);
-
-
-
-
-
 
 
     //管理画面
-function manage_article_posts_columns($columns) {
+function manage_item_posts_columns($columns) {
     $columns = array(
         'cb' => '<input type="checkbox" />',
         'title' => 'タイトル',
         'articlecat' => 'カテゴリー',
-        'articletag' => '関連タグ',
-        'author' => '投稿者',
         'thumbnail' => 'サムネイル',
-        'author' => '投稿者',
-        'date' => '投稿日時',
-        'modified_date' => '最終更新日',
     );
     return $columns;
 }
@@ -260,8 +140,8 @@ function custom_search_template($template){
     //タイトルプレースホルダーを変更
 function change_default_title( $title ) {
     $screen = get_current_screen();
-    if ( $screen -> post_type == 'tour' ) {
-        $title = 'ツアーのタイトルを入力';
+    if ( $screen -> post_type == 'item' ) {
+        $title = '日本語の商品名を入力';
     }
     return $title;
 }
