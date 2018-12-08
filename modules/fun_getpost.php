3456ffc41590e423bc2e9b51ecc529418bc0041c
hacca <horiuchi@hacca.cc>
2018-12-08 12:13:03 +0900

    no message

--- modules/fun_getpost.php
+++ modules/fun_getpost.php
@@ -11,7 +11,12 @@
             $query->set( 'post_type', 'item' );
             $query->set( 'posts_per_page', -1 );
             $query->set( 'ignore_sticky_posts', 1 );
-            $query->set( 'orderby', 'term_order' );
+            $query->set( 'orderby', array(
+                'meta_value' => 'desc',
+                'term_order' => 'asc',
+                'date' => 'asc',
+                )
+            );
             $query->set( 'order', 'ASC' );
             $query->set( 'tax_query',
                 array(
@@ -24,20 +29,50 @@
                     ),
                 )
             );
+            $query->set( 'meta_query',
+                array(
+                    'relation' => 'AND',
+                    array(
+                        'key'=>'item_sold__out',
+                        'value' => array('販売中','売り切れ'),
+                        'operator' => 'IN'
+                    ),
+                )
+            );
         return;
         }
         if ( $query->is_post_type_archive( 'item' ) ) {
             $query->set( 'posts_per_page', -1 );
             $query->set( 'ignore_sticky_posts', 1 );
-            $query->set( 'orderby', 'date' );
+            $query->set( 'orderby', 'meta_value date' );
             $query->set( 'order', 'desc' );
+            $query->set( 'meta_query',
+                array(
+                    'relation' => 'AND',
+                    array(
+                        'key'=>'item_sold__out',
+                        'value' => array('販売中','売り切れ'),
+                        'operator' => 'IN'
+                    ),
+                )
+            );
             return;
         }
         if ( $query->is_tax( 'itemcat','' ) ) {
             $query->set( 'posts_per_page', -1 );
             $query->set( 'ignore_sticky_posts', 1 );
-            $query->set( 'orderby', 'date' );
+            $query->set( 'orderby', 'meta_value date' );
             $query->set( 'order', 'desc' );
+            $query->set( 'meta_query',
+                array(
+                    'relation' => 'AND',
+                    array(
+                        'key'=>'item_sold__out',
+                        'value' => array('販売中','売り切れ'),
+                        'operator' => 'IN'
+                    ),
+                )
+            );
             return;
         }
     }
